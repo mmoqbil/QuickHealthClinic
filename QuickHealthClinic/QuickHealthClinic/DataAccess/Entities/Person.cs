779970250687320f7d405eb407d572e35cde53fa
Mateusz <mateusz.moqbil@gmail.com>
2023-08-17 14:13:18 +0200

    Added string.Empty to all string properties in Person class

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Person.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Person.cs
@@ -3,11 +3,11 @@
     public abstract class Person
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public DateTime Created { get; }
-        public string AvatarUri { get; set; }
+        public string AvatarUri { get; set; } = string.Empty;
     }
 }
