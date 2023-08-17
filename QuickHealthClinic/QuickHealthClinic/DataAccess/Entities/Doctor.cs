9436b399fb72ef9f99cb435d780840eaff416ab1
Mateusz <mateusz.moqbil@gmail.com>
2023-08-17 14:07:08 +0200

    Added Doctor class

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickHealthClinic.DataAccess.Entities
+{
+    public class Doctor : Person
+    {
+        public string Specialist { get; set; }
+        public string Description { get; set; }
+        public string Education { get; set; }
+        public string Services { get; set; }
+    }
+}
