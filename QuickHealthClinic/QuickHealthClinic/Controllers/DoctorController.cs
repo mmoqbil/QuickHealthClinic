2dec8ae6ddef3bae3c07c9561290b144b6c45f21
Mateusz <mateusz.moqbil@gmail.com>
2023-08-17 23:08:02 +0200

    Added DoctorController

---
+++ QuickHealthClinic/QuickHealthClinic/Controllers/DoctorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickHealthClinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+    }
+}
