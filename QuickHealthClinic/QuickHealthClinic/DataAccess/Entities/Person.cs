7a6f08235f4a0b863ba7fc96ff409caed907bbe8
Mateusz <mateusz.moqbil@gmail.com>
2023-08-17 14:04:12 +0200

    Created Person abstract class with properties.

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace QuickHealthClinic.DataAccess.Entities
+{
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; }
+        public string AvatarUri { get; set; }
+    }
+}
