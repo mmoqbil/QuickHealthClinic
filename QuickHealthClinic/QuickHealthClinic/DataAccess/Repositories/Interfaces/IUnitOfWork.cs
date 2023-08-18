99f78bb0df3fc7e6a2e5b352fe6edfbef48a6004
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 19:06:32 +0200

    Added IUnitOfWork with DoctorRepository property and  SaveAsync method

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickHealthClinic.DataAccess.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IDoctorRepository DoctorRepository { get; }
+        Task SaveAsync();
+    }
+}
