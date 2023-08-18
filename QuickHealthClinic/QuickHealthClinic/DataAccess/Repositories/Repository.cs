f09f58a5fe9c4976e6dd54c9833533c28a84580c
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 19:10:46 +0200

    Added DbContext to Repository abstract class

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Repository.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Repository.cs
@@ -1,10 +1,14 @@
-﻿using QuickHealthClinic.DataAccess.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using QuickHealthClinic.DataAccess.DbContexts;
+using QuickHealthClinic.DataAccess.Repositories.Interfaces;
 using System.Linq.Expressions;
 
 namespace QuickHealthClinic.DataAccess.Repositories
 {
     public abstract class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly QuickHealthClinicContext Context;
+        protected DbSet<T> DbSet;
         public Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string? includeProperties = null)
         {
             throw new NotImplementedException();
