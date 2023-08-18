343dd1f84a70f31b8d2e07a43a9d8e9ccda75309
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 18:59:15 +0200

    Added GetAllAsync generic method in IRepository

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace QuickHealthClinic.DataAccess.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string? includeProperties = null);
+    }
+}
