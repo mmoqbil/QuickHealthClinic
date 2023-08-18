06c8e7c36d307179d9735d3af712799e9d2cdd97
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 17:22:06 +0200

    Added DbContext to Program.cs

--- QuickHealthClinic/QuickHealthClinic/Program.cs
+++ QuickHealthClinic/QuickHealthClinic/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using QuickHealthClinic.DataAccess.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<QuickHealthClinicContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QuickHealthClinicConnection"));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
