942f19e01f09f5371a1fbc84ce635d1ede320c2b
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 19:41:46 +0200

    Added UnitOfWork class

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using QuickHealthClinic.DataAccess.Repositories.Interfaces;
+
+namespace QuickHealthClinic.DataAccess.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IDoctorRepository DoctorRepository => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
