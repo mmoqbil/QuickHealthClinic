48cc7f929eb68b56b1af341b8d9caeda54fd5930
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 19:08:47 +0200

    Added Repository abstract class that implements by IRepository interface

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Repository.cs
@@ -0,0 +1,13 @@
+﻿using QuickHealthClinic.DataAccess.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace QuickHealthClinic.DataAccess.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        public Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
