5d57d4428ed10efadf9db369a6168a5782daa464
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 09:18:00 +0200

    Added Doctors table to Db

--- QuickHealthClinic/QuickHealthClinic/DataAccess/DbContexts/QuickHealthClinicContext.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/DbContexts/QuickHealthClinicContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using QuickHealthClinic.DataAccess.Entities;
 
 namespace QuickHealthClinic.DataAccess.DbContexts
 {
     public class QuickHealthClinicContext : DbContext
     {
+        public QuickHealthClinicContext(DbContextOptions<QuickHealthClinicContext> options) : base(options)
+        {
+        }
+        public DbSet<Doctor> Doctors { get; set; }
     }
 }
