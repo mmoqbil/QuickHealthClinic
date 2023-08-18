f9dfdd7dc34f03b24e1bf6f60004340cf77feb71
Mateusz <mateusz.moqbil@gmail.com>
2023-08-18 19:40:09 +0200

    Added constructor to DoctorRepository

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/DoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuickHealthClinic.DataAccess.DbContexts;
+using QuickHealthClinic.DataAccess.Entities;
+using QuickHealthClinic.DataAccess.Repositories.Interfaces;
+
+namespace QuickHealthClinic.DataAccess.Repositories
+{
+    public class DoctorRepository : Repository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(QuickHealthClinicContext context) : base(context)
+        {
+            DbSet = context.Doctors;
+        }
+    }
+}
