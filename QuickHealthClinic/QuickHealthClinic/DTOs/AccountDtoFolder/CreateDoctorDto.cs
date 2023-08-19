6509cf7e8f4ccd489ecea96cd07ba146729ecd91
Mateusz <mateusz.moqbil@gmail.com>
2023-08-19 12:36:36 +0200

    Added CreateDoctorDto with all needed properties

---
+++ QuickHealthClinic/QuickHealthClinic/DTOs/AccountDtoFolder/CreateDoctorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace QuickHealthClinic.DTOs.AccountDtoFolder
+{
+    public class CreateDoctorDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string Specialist { get; set; }
+        public string PasswordHash { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
