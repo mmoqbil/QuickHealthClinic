940b489f0415f4017fccca32ca43432b858b0bc5
Mateusz <mateusz.moqbil@gmail.com>
2023-08-19 12:52:52 +0200

    Added GetDoctorsAsync method to IDoctorService interface

--- QuickHealthClinic/QuickHealthClinic/Services/DoctorServices/IDoctorService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/DoctorServices/IDoctorService.cs
@@ -1,6 +1,9 @@
-﻿namespace QuickHealthClinic.Services.DoctorServices
+﻿using QuickHealthClinic.DTOs.DoctorDtoFolder;
+
+namespace QuickHealthClinic.Services.DoctorServices
 {
     public interface IDoctorService
     {
+        Task<IEnumerable<DoctorDto>> GetDoctorsAsync();
     }
 }
