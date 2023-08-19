d20e0ad75caa7b7490ebdc6420662ef85b39b984
Mateusz <mateusz.moqbil@gmail.com>
2023-08-19 12:44:25 +0200

    Created Mapper for DoctorDto

--- QuickHealthClinic/QuickHealthClinic/Configurations/Mapper/MappingProfile.cs
+++ QuickHealthClinic/QuickHealthClinic/Configurations/Mapper/MappingProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using QuickHealthClinic.DataAccess.Entities;
+using QuickHealthClinic.DTOs.DoctorDtoFolder;
 
 namespace QuickHealthClinic.Configurations.Mapper
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Doctor, DoctorDto>();
+        }
     }
 }
