e6b78adac0178e5c645142a2f2575f0b7dadf840
Mateusz <mateusz.moqbil@gmail.com>
2023-08-19 12:56:25 +0200

    Added mapper to DoctorService

---
+++ QuickHealthClinic/QuickHealthClinic/Services/DoctorServices/DoctorService.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using QuickHealthClinic.DataAccess.Repositories.Interfaces;
+using QuickHealthClinic.DTOs.DoctorDtoFolder;
+
+namespace QuickHealthClinic.Services.DoctorServices
+{
+    public class DoctorService : IDoctorService
+    {
+        private readonly IMapper _mapper;
+        public Task<IEnumerable<DoctorDto>> GetDoctorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
