34f90bfcfef93677e53d89ec899be8432f8af4cc
Mateusz <mateusz.moqbil@gmail.com>
2023-08-21 17:13:42 +0200

    Added DoctorService to DoctorController

--- QuickHealthClinic/QuickHealthClinic/Controllers/DoctorController.cs
+++ QuickHealthClinic/QuickHealthClinic/Controllers/DoctorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using QuickHealthClinic.Services.DoctorServices;
 
 namespace QuickHealthClinic.Controllers
 {
@@ -7,5 +8,6 @@ namespace QuickHealthClinic.Controllers
     [ApiController]
     public class DoctorController : ControllerBase
     {
+        private readonly IDoctorService _doctorService;
     }
 }
