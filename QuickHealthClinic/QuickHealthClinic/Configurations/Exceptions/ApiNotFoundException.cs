b1448493f87d0ff3e9adce26919c4836f8da6818
Mateusz <mateusz.moqbil@gmail.com>
2023-08-22 18:37:20 +0200

    Now ApiNotFoundExceptions class can throw an exception

--- QuickHealthClinic/QuickHealthClinic/Configurations/Exceptions/ApiNotFoundException.cs
+++ QuickHealthClinic/QuickHealthClinic/Configurations/Exceptions/ApiNotFoundException.cs
@@ -1,6 +1,17 @@
 ﻿namespace QuickHealthClinic.Configurations.Exceptions
 {
-    public class NotFoundApiException
+    public class ApiNotFoundException : ApiException
     {
+        public ApiNotFoundException()
+        {
+        }
+
+        public ApiNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ApiNotFoundException(string resource, string key) : base($"{resource} with key {key} not found.")
+        {
+        }
     }
 }
