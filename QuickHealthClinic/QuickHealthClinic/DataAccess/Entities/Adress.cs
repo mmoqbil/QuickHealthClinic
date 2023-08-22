c2dc0f6d4621885cac096936c55a6b3ceb646561
Mateusz <mateusz.moqbil@gmail.com>
2023-08-22 14:56:20 +0200

    Added properties to Adress entity

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Adress.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Adress.cs
@@ -1,6 +1,11 @@
 ﻿namespace QuickHealthClinic.DataAccess.Entities
 {
-    public class Adress
+    public class Address
     {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public virtual Doctor Doctor { get; set; }
     }
 }
