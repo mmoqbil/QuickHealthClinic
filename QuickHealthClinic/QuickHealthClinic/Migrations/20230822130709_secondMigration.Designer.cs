9cf453c9dcddcc4ea671c636bd2deef1e3fff6e8
Mateusz <mateusz.moqbil@gmail.com>
2023-08-22 15:08:39 +0200

    Added Migration with Adress table

--- QuickHealthClinic/QuickHealthClinic/Migrations/20230822130709_secondMigration.Designer.cs
+++ QuickHealthClinic/QuickHealthClinic/Migrations/20230822130709_secondMigration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuickHealthClinic.DataAccess.DbContexts;
 
@@ -10,9 +11,11 @@ using QuickHealthClinic.DataAccess.DbContexts;
 namespace QuickHealthClinic.Migrations
 {
     [DbContext(typeof(QuickHealthClinicContext))]
-    partial class QuickHealthClinicContextModelSnapshot : ModelSnapshot
+    [Migration("20230822130709_secondMigration")]
+    partial class secondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ QuickHealthClinic/QuickHealthClinic/Migrations/20230822130709_secondMigration.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickHealthClinic.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "Doctors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_AddressId",
+                table: "Doctors",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Address_AddressId",
+                table: "Doctors",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Address_AddressId",
+                table: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_AddressId",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Doctors");
+        }
+    }
+}
