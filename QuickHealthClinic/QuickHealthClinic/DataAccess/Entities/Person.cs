6c93e9e95847838d344168a2051b5f7270c0f9c3
Mateusz <mateusz.moqbil@gmail.com>
2023-08-22 14:57:26 +0200

    Added AdressId and Adress propierties to Person abstract class

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Person.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Person.cs
@@ -7,6 +7,8 @@
         public string LastName { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public int AddressId { get; set; }
+        public virtual Address Address { get; set; }
         public DateTime Created { get; }
         public string AvatarUri { get; set; } = string.Empty;
     }
