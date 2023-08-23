0d77fb2d3c81093de3f32108273bb92f7e57a0b1
Mateusz <mateusz.moqbil@gmail.com>
2023-08-23 12:47:44 +0200

    Added AccountController with route api/account

---
+++ QuickHealthClinic/QuickHealthClinic/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickHealthClinic.Controllers
+{
+    [ApiController]
+    [Route("/api/account")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
