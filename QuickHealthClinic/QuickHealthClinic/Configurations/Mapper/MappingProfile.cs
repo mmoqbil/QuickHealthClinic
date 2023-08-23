114e802c15a01255bb506c3534d3c2a8041e79eb
Mateusz <mateusz.moqbil@gmail.com>
2023-08-23 12:21:07 +0200

    Now Mapping from CreateDoctorDto properties to new Adress entity

--- QuickHealthClinic/QuickHealthClinic/Configurations/Mapper/MappingProfile.cs
+++ QuickHealthClinic/QuickHealthClinic/Configurations/Mapper/MappingProfile.cs
@@ -10,11 +10,14 @@ namespace QuickHealthClinic.Configurations.Mapper
         public MappingProfile()
         {
             CreateMap<Doctor, DoctorDto>()
-            .ForMember(dto => dto.City, d => d.MapFrom(a => a.Address.City))
-            .ForMember(dto => dto.Street, d => d.MapFrom(a => a.Address.Street))
-            .ForMember(dto => dto.PostalCode, d => d.MapFrom(a => a.Address.PostalCode));
+                .ForMember(dto => dto.City, d => d.MapFrom(a => a.Address.City))
+                .ForMember(dto => dto.Street, d => d.MapFrom(a => a.Address.Street))
+                .ForMember(dto => dto.PostalCode, d => d.MapFrom(a => a.Address.PostalCode));
 
-            CreateMap<CreateDoctorDto, Doctor>();
+            CreateMap<CreateDoctorDto, Doctor>()
+                .ForMember(d => d.Address,
+                    c => c.MapFrom(dto => new Address
+                        { City = dto.City, Street = dto.Street, PostalCode = dto.PostalCode })).ReverseMap();
         }
     }
 }
