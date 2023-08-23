9ea732565efabe5c1c463ace716d8b8272f6c46d
Mateusz <mateusz.moqbil@gmail.com>
2023-08-23 14:02:18 +0200

    Now if userId equals doctorId you get succesful authorization

--- QuickHealthClinic/QuickHealthClinic/Authorization/DoctorResourceOperationRequirementHandler.cs
+++ QuickHealthClinic/QuickHealthClinic/Authorization/DoctorResourceOperationRequirementHandler.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using QuickHealthClinic.DataAccess.Entities;
+using System.Security.Claims;
 
 namespace QuickHealthClinic.Authorization
 {
     public class DoctorResourceOperationRequirementHandler : AuthorizationHandler<ResourceOperationRequirement, Doctor>
     {
-        
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        ResourceOperationRequirement requirement,
+        Doctor doctor)
+        {
+            if (requirement.ResourceOperation is ResourceOperation.Read or ResourceOperation.Create)
+                context.Succeed(requirement);
+
+            var userId = context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            if (doctor.Id == int.Parse(userId)) context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
     }
 }
