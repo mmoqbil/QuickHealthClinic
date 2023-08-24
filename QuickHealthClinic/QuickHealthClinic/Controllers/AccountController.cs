2581686613be7adde4c1d6717cc4a92bcfb5dc37
Mateusz <mateusz.moqbil@gmail.com>
2023-08-24 18:11:22 +0200

    Finally changed every doctor to mentor according to new project vision

--- QuickHealthClinic/QuickHealthClinic/Controllers/AccountController.cs
+++ QuickHealthClinic/QuickHealthClinic/Controllers/AccountController.cs
@@ -8,30 +8,30 @@ namespace QuickLifeCoachingClinic.Controllers
     [Route("/api/account")]
     public class AccountController : Controller
     {
-        private readonly IMentorService _doctorService;
-        public AccountController(IMentorService doctorService)
+        private readonly IMentorService _mentorService;
+        public AccountController(IMentorService mentorService)
         {
-            _doctorService = doctorService;
+            _mentorService = mentorService;
         }
 
         [HttpPost("mentor/register")]
         public async Task<IActionResult> AddDoctorAsync([FromBody] CreateMentorDto dto)
         {
-            var (mentorId, mentor) = await _doctorService.CreateMentorAsync(dto);
+            var (mentorId, mentor) = await _mentorService.CreateMentorAsync(dto);
             return Created($"/api/mentors/{mentorId}", mentor);
         }
 
         [HttpPut("mentor/{id}")]
         public async Task<IActionResult> UpdateDoctorAsync([FromRoute] int id, [FromBody] UpdateMentorDto dto)
         {
-            await _doctorService.UpdateMentorAsync(id, dto);
+            await _mentorService.UpdateMentorAsync(id, dto);
             return NoContent();
         }
 
         [HttpDelete("mentor/{id}")]
         public async Task<IActionResult> DeleteMentorByIdAsync([FromRoute] int id)
         {
-            await _doctorService.DeleteMentorAsync(id);
+            await _mentorService.DeleteMentorAsync(id);
             return NoContent();
         }
     }
