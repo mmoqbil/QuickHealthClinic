e4220347943646c8d33ec7462c7c9fc9059668ab
Mateusz <mateusz.moqbil@gmail.com>
2023-08-24 11:28:47 +0200

    refactored from doctor to mentor

--- QuickHealthClinic/QuickHealthClinic/Authorization/MentorResourceOperationRequirementHandler.cs
+++ QuickHealthClinic/QuickHealthClinic/Authorization/MentorResourceOperationRequirementHandler.cs
@@ -8,13 +8,13 @@ namespace QuickHealthClinic.Authorization
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
         ResourceOperationRequirement requirement,
-        Mentor doctor)
+        Mentor mentor)
         {
             if (requirement.ResourceOperation is ResourceOperation.Read or ResourceOperation.Create)
                 context.Succeed(requirement);
 
             var userId = context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            if (doctor.Id == int.Parse(userId)) context.Succeed(requirement);
+            if (mentor.Id == int.Parse(userId)) context.Succeed(requirement);
 
             return Task.CompletedTask;
         }
