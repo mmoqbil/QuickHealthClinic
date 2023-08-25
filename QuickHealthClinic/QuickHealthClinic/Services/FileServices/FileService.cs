049bdca61fd56b04372993ef15c123e9cc5f2fbc
Mateusz <mateusz.moqbil@gmail.com>
2023-08-25 18:19:57 +0200

    Created FileService that implements after IFileService interface

---
+++ QuickHealthClinic/QuickHealthClinic/Services/FileServices/FileService.cs
@@ -0,0 +1,12 @@
+﻿using QuickLifeCoachingClinic.DTOs.ImageDto;
+
+namespace QuickLifeCoachingClinic.Services.FileServices
+{
+    public class FileService : IFileService
+    {
+        public Task<string> SaveFile(CreateImageDto file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
