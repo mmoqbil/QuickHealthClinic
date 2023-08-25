200d74a0903147ff2851d0cfc9814543d9485c1a
Mateusz <mateusz.moqbil@gmail.com>
2023-08-25 10:43:04 +0200

    Added Certificate class

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Certificate.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickLifeCoachingClinic.DataAccess.Entities
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+        public virtual Mentor Mentor { get; set; }
+        public string Filename { get; set; } = null!;
+    }
+}
