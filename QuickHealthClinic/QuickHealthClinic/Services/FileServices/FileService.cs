5e133be4a3a830a4c580a0c2add3b2e64bbc950e
Mateusz <mateusz.moqbil@gmail.com>
2023-08-25 18:23:43 +0200

    Added WebHostEnviroment field to FileService

--- QuickHealthClinic/QuickHealthClinic/Services/FileServices/FileService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/FileServices/FileService.cs
@@ -4,6 +4,13 @@ namespace QuickLifeCoachingClinic.Services.FileServices
 {
     public class FileService : IFileService
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         public Task<string> SaveFile(CreateImageDto file)
         {
             throw new NotImplementedException();
