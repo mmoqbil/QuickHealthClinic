b19f24afeafcd7100de0ed407ef9ebf66dacb619
Mateusz <mateusz.moqbil@gmail.com>
2023-08-25 18:57:38 +0200

    Added properties and fields to the Clinic class

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Clinic.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Clinic.cs
@@ -2,5 +2,11 @@
 {
     public class Clinic
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int AddressId { get; set; }
+        public virtual Address Address { get; set; }
+        public virtual List<Mentor> Mentors { get; set; } = new();
     }
 }
