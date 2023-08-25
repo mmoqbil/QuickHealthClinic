48c18f176dfc74cc47ebc4cbbe9c5a36fe3371d0
Mateusz <mateusz.moqbil@gmail.com>
2023-08-25 18:17:59 +0200

    Added IFileService interface to with SaveFile method

---
+++ QuickHealthClinic/QuickHealthClinic/Services/FileServices/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using QuickLifeCoachingClinic.DTOs.ImageDto;
+
+namespace QuickLifeCoachingClinic.Services.FileServices
+{
+    public interface IFileService
+    {
+        Task<string> SaveFile(CreateImageDto file);
+    }
+}
