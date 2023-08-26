5f2f2136858b1b10d966a25bb83014f2674b5e94
Mateusz <mateusz.moqbil@gmail.com>
2023-08-26 20:34:03 +0200

    Added mapper and unitOfWork to Dependency Injection contener

--- QuickHealthClinic/QuickHealthClinic/Services/ClinicService/ClinicService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/ClinicService/ClinicService.cs
@@ -8,6 +8,11 @@ namespace QuickLifeCoachingClinic.Services.ClinicService
     {
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
+        public ClinicService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
         public Task<IEnumerable<ClinicDto>> GetClinicAsync()
         {
             throw new NotImplementedException();
