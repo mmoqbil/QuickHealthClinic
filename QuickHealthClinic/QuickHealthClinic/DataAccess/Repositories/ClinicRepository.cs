ba3ffb645dcab415f1297fbcaa6de988d2779ea8
Mateusz <mateusz.moqbil@gmail.com>
2023-08-26 20:43:13 +0200

    Created ClinicRepository that inherits from Repository and implements after IClinicRepository interface

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/ClinicRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuickLifeCoachingClinic.DataAccess.DbContexts;
+using QuickLifeCoachingClinic.DataAccess.Entities;
+using QuickLifeCoachingClinic.DataAccess.Repositories.Interfaces;
+
+namespace QuickLifeCoachingClinic.DataAccess.Repositories
+{
+    public class ClinicRepository : Repository<Clinic>, IClinicRepository
+    {
+        public ClinicRepository(QuickLifeCoachingClinicContext context) : base(context)
+        {
+            DbSet = context.Clinics;
+        }
+    }
+}
