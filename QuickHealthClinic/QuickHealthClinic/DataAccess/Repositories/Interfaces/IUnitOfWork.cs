dc5800fbe2791994121587aca18ecc1071242a1d
Mateusz <mateusz.moqbil@gmail.com>
2023-08-26 20:45:24 +0200

    Added ClinicRepository field to IUnitOfWork interface

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Interfaces/IUnitOfWork.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/Interfaces/IUnitOfWork.cs
@@ -3,6 +3,7 @@
     public interface IUnitOfWork : IDisposable
     {
         IMentorRepository MentorRepository { get; }
+        IClinicRepository ClinicRepository { get; }
         Task SaveAsync();
     }
 }
