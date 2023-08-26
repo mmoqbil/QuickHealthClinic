02edb785c0a339febb9ec6e298bdab3d57e0529e
Mateusz <mateusz.moqbil@gmail.com>
2023-08-26 20:32:15 +0200

    Created ClinicService class that implements after IClinicService interface

---
+++ QuickHealthClinic/QuickHealthClinic/Services/ClinicService/ClinicService.cs
@@ -0,0 +1,12 @@
+﻿using QuickLifeCoachingClinic.DTOs.ClinicDto;
+
+namespace QuickLifeCoachingClinic.Services.ClinicService
+{
+    public class ClinicService : IClinicService
+    {
+        public Task<IEnumerable<ClinicDto>> GetClinicAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
