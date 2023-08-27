3194cefc5a1299366472d360e53c6d96c48e1552
Mateusz <mateusz.moqbil@gmail.com>
2023-08-27 13:54:37 +0200

    Added GetClinicByIdAsync method in IClinicService interface

--- QuickHealthClinic/QuickHealthClinic/Services/ClinicService/IClinicService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/ClinicService/IClinicService.cs
@@ -5,5 +5,6 @@ namespace QuickLifeCoachingClinic.Services.ClinicService
     public interface IClinicService
     {
         Task<IEnumerable<ClinicDto>> GetClinicAsync();
+        Task<ClinicDto> GetClinicByIdAsync(int id);
     }
 }
