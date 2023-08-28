b42e82f1ed70649ef93fd8971d87be3267fd15c2
Mateusz <mateusz.moqbil@gmail.com>
2023-08-28 14:55:32 +0200

    Filled UpdateClinicDto with dto's properties

--- QuickHealthClinic/QuickHealthClinic/DTOs/ClinicDto/UpdateClinicDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/ClinicDto/UpdateClinicDto.cs
@@ -2,5 +2,10 @@
 {
     public class UpdateClinicDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
     }
 }
