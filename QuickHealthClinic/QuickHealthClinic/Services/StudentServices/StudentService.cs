a13dd0cb0a3f402a4422c79fb3e33e54cf0eeed8
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 17:11:52 +0200

    Added context, mapper and unitOfWork fields to StudentService

--- QuickHealthClinic/QuickHealthClinic/Services/StudentServices/StudentService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/StudentServices/StudentService.cs
@@ -1,9 +1,15 @@
-﻿using QuickLifeCoachingClinic.DTOs.StudentDtoFolder;
+﻿using AutoMapper;
+using QuickLifeCoachingClinic.DataAccess.DbContexts;
+using QuickLifeCoachingClinic.DataAccess.Repositories.Interfaces;
+using QuickLifeCoachingClinic.DTOs.StudentDtoFolder;
 
 namespace QuickLifeCoachingClinic.Services.StudentServices
 {
     public class StudentService : IStudentService
     {
+        private readonly QuickLifeCoachingClinicContext _context;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
         public async Task<IEnumerable<StudentDto>> GetStudentsAsync()
         {
             throw new NotImplementedException();
