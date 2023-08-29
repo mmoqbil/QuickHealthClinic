5810886f6dccaee2f753c635891f7836ed69c1e1
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 17:18:00 +0200

    Created StudentController

---
+++ QuickHealthClinic/QuickHealthClinic/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickLifeCoachingClinic.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
