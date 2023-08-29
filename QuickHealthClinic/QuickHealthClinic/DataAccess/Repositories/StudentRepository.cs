33e495128a7e2ab660473a38c0c8650f33aed575
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 16:52:00 +0200

    Added Students table to context

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/StudentRepository.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/StudentRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace QuickLifeCoachingClinic.DataAccess.Repositories
+﻿using QuickLifeCoachingClinic.DataAccess.DbContexts;
+using QuickLifeCoachingClinic.DataAccess.Entities;
+using QuickLifeCoachingClinic.DataAccess.Repositories.Interfaces;
+
+namespace QuickLifeCoachingClinic.DataAccess.Repositories
 {
-    public class StudentRepository
+    public class StudentRepository : Repository<Student>, IStudentRepository
     {
+        public StudentRepository(QuickLifeCoachingClinicContext context) : base(context)
+        {
+            DbSet = context.Students;
+        }
     }
 }
