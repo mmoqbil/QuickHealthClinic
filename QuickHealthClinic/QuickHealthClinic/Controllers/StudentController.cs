17218fa33a73c10a296d7364e8bbc5be0106fb31
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 17:22:15 +0200

    Injected StudentService and rename private name of it to _studentService

--- QuickHealthClinic/QuickHealthClinic/Controllers/StudentController.cs
+++ QuickHealthClinic/QuickHealthClinic/Controllers/StudentController.cs
@@ -7,7 +7,11 @@ namespace QuickLifeCoachingClinic.Controllers
     [Route("/api/students")]
     public class StudentController : Controller
     {
-        private readonly IStudentService studentService;
+        private readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
         public IActionResult Index()
         {
             return View();
