4805a1a093c9ac516d2da876684880aedb71b3e4
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 17:05:16 +0200

    StudentService now implements after IStudentService interface

--- QuickHealthClinic/QuickHealthClinic/Services/StudentServices/StudentService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/StudentServices/StudentService.cs
@@ -1,6 +1,12 @@
-﻿namespace QuickLifeCoachingClinic.Services.StudentServices
+﻿using QuickLifeCoachingClinic.DTOs.StudentDtoFolder;
+
+namespace QuickLifeCoachingClinic.Services.StudentServices
 {
-    public class StudentService
+    public class StudentService : IStudentService
     {
+        public async Task<IEnumerable<StudentDto>> GetStudentsAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
