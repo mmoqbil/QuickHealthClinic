720d1d14722fd775a3fa81735b25ac025c96e5ae
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 16:43:45 +0200

    Implemented student class with properties and fields

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Student.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Student.cs
@@ -1,6 +1,10 @@
 ﻿namespace QuickLifeCoachingClinic.DataAccess.Entities
 {
-    public class Student
+    public class Student : Person
     {
+        public string Pesel { get; set; }
+        public virtual List<Clinic> Clinics { get; set; } = new();
+        public virtual List<Mentor> Doctors { get; set; } = new();
+        public virtual Role Role { get; set; } = Role.Student;
     }
 }
