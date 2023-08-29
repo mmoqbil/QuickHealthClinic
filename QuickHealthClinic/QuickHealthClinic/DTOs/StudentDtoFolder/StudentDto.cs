645502dfe7698c2e97bbc392295582533c561646
Mateusz <mateusz.moqbil@gmail.com>
2023-08-29 17:01:44 +0200

    Filled StudentDto with all dto's properties needed

--- QuickHealthClinic/QuickHealthClinic/DTOs/StudentDtoFolder/StudentDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/StudentDtoFolder/StudentDto.cs
@@ -2,5 +2,14 @@
 {
     public class StudentDto
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string Pesel { get; set; }
     }
 }
