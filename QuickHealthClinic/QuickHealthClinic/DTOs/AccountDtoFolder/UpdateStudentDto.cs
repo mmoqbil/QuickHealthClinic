9102c01ee795896103f5482153f295b5ef6b3266
Mateusz <mateusz.moqbil@gmail.com>
2023-08-30 18:57:32 +0200

    Filled UpdateStudentDto with dto's properties like name, phone number and email

--- QuickHealthClinic/QuickHealthClinic/DTOs/AccountDtoFolder/UpdateStudentDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/AccountDtoFolder/UpdateStudentDto.cs
@@ -1,6 +1,14 @@
-﻿namespace QuickLifeCoachingClinic.DTOs.AccountDtoFolder
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickLifeCoachingClinic.DTOs.AccountDtoFolder
 {
     public class UpdateStudentDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Phone] public string PhoneNumber { get; set; }
+
+        [EmailAddress] public string Email { get; set; }
     }
 }
