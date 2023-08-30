6a8ca0677b4b7c75d49a400fae5eb609362ad29e
Mateusz <mateusz.moqbil@gmail.com>
2023-08-30 13:22:16 +0200

    Filled CreateStudentDto with dto's properties like name,city,street etc.

--- QuickHealthClinic/QuickHealthClinic/DTOs/AccountDtoFolder/CreateStudentDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/AccountDtoFolder/CreateStudentDto.cs
@@ -2,5 +2,15 @@
 {
     public class CreateStudentDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string Pesel { get; set; }
+        public string PasswordHash { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
