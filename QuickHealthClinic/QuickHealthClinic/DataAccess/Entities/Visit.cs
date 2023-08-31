bcf62aebe573df28978b669ea625b05f03e757e8
Mateusz <mateusz.moqbil@gmail.com>
2023-08-31 14:57:45 +0200

    Filled Visit class with properties like visit time and duration and fields like student and mentor having visit

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Visit.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Visit.cs
@@ -2,5 +2,14 @@
 {
     public class Visit
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+        public int MentorId { get; set; }
+        public virtual Mentor Mentor { get; set; }
+        public DateTime VisitDate { get; set; }
+        public int Duration { get; set; }
+        public bool? Confirmed { get; set; } = null;
     }
 }
