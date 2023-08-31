f72d822785fa30c792588b00ec5e533521bfc162
Mateusz <mateusz.moqbil@gmail.com>
2023-08-31 15:23:39 +0200

    Declared GetVisitsForMonth method in IVisitService interface

--- QuickHealthClinic/QuickHealthClinic/Services/VisitServices/IVisitService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/VisitServices/IVisitService.cs
@@ -1,6 +1,9 @@
-﻿namespace QuickLifeCoachingClinic.Services.VisitServices
+﻿using QuickLifeCoachingClinic.DTOs.VisitDtoFolder;
+
+namespace QuickLifeCoachingClinic.Services.VisitServices
 {
     public interface IVisitService
     {
+        Task<IEnumerable<VisitCalendarDto>> GetVisitsForMonth(int mentorId, DateOnly date);
     }
 }
