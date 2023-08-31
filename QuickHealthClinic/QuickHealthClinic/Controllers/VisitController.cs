9862b372a7d452dd0c6633dba2e18995a70b6353
Mateusz <mateusz.moqbil@gmail.com>
2023-08-31 15:35:11 +0200

    Created VisitController

---
+++ QuickHealthClinic/QuickHealthClinic/Controllers/VisitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickLifeCoachingClinic.Controllers
+{
+    public class VisitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
