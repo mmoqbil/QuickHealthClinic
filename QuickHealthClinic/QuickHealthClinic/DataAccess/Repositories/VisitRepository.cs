688c14319aa219f9fe94045b27f8edf74150b52a
Mateusz <mateusz.moqbil@gmail.com>
2023-08-31 15:18:33 +0200

    Implemented GetVisitForMonth method to get exactly day and visits in month

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/VisitRepository.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Repositories/VisitRepository.cs
@@ -1,6 +1,8 @@
-﻿using QuickLifeCoachingClinic.DataAccess.DbContexts;
+﻿using Microsoft.EntityFrameworkCore;
+using QuickLifeCoachingClinic.DataAccess.DbContexts;
 using QuickLifeCoachingClinic.DataAccess.Entities;
 using QuickLifeCoachingClinic.DataAccess.Repositories.Interfaces;
+using QuickLifeCoachingClinic.DTOs.VisitDtoFolder;
 
 namespace QuickLifeCoachingClinic.DataAccess.Repositories
 {
@@ -9,6 +11,26 @@ namespace QuickLifeCoachingClinic.DataAccess.Repositories
         public VisitRepository(QuickLifeCoachingClinicContext context) : base(context)
         {
             DbSet = context.Visits;
+        }
+
+        public async Task<IReadOnlyList<VisitCalendarDto>> GetVisitsForMonth(int mentorId, DateOnly date)
+        {
+            var visitDates = await DbSet
+                .Where(visit => visit.MentorId == mentorId)
+                .Where(visit => visit.VisitDate.Month == date.Month && visit.VisitDate.Year == date.Year)
+                .Select(visit => visit.VisitDate)
+                .ToListAsync();
+
+            var visitCalendarDto = visitDates
+                .GroupBy(visit => visit.Day)
+                .Select(group => new VisitCalendarDto
+                {
+                    Day = group.Key,
+                    Visits = group.Count()
+                })
+                .ToList();
+
+            return visitCalendarDto;
         }
     }
 }
