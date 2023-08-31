a6d07f999326f0620d2388d9851f74cdb4bdfce8
Mateusz <mateusz.moqbil@gmail.com>
2023-08-31 15:35:53 +0200

    Added route for api/visits in VisitController

--- QuickHealthClinic/QuickHealthClinic/Controllers/VisitController.cs
+++ QuickHealthClinic/QuickHealthClinic/Controllers/VisitController.cs
@@ -2,6 +2,8 @@
 
 namespace QuickLifeCoachingClinic.Controllers
 {
+    [ApiController]
+    [Route("/api/visits")]
     public class VisitController : Controller
     {
         public IActionResult Index()
