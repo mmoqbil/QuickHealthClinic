800d9d908755627450a5ff4fa92af1c9c45a3e39
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 15:36:24 +0200

    Declared GetMentorVisitForDate in IVisitService interface

--- QuickHealthClinic/QuickHealthClinic/Services/VisitServices/IVisitService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/VisitServices/IVisitService.cs
@@ -1,9 +1,11 @@
-﻿using QuickLifeCoachingClinic.DTOs.VisitDtoFolder;
+﻿using QuickLifeCoachingClinic.DataAccess.Entities;
+using QuickLifeCoachingClinic.DTOs.VisitDtoFolder;
 
 namespace QuickLifeCoachingClinic.Services.VisitServices
 {
     public interface IVisitService
     {
         Task<IEnumerable<VisitCalendarDto>> GetVisitsForMonth(int mentorId, DateOnly date);
+        Task<Visit?> GetMentorVisitForDate(int mentorId, DateTime startDate, DateTime endDate);
     }
 }
