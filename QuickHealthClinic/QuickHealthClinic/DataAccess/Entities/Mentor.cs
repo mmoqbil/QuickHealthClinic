3ca402eb40bfa76fc23bb261b1416450127fac77
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 16:47:50 +0200

    Added Students and Clinics lists to the Mentor model

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Mentor.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Mentor.cs
@@ -7,6 +7,8 @@ namespace QuickLifeCoachingClinic.DataAccess.Entities
         public string Description { get; set; } = string.Empty;
         public string Education { get; set; } = string.Empty;
         public string Services { get; set; } = string.Empty;
+        public virtual List<Clinic> Clinics { get; set; } = new();
+        public virtual List<Student> Students { get; set; } = new();
         public virtual Role Role { get; set; } = Role.Mentor;
         public List<Certificate> Certificates { get; set; } = new();
     }
