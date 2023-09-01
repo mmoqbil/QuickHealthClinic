7dfd9e3400aae633d8d704bb9d0394f6531ef67b
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 16:54:37 +0200

    Created relation using entityframework in MentorConfiguration

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/MentorConfiguration.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/MentorConfiguration.cs
@@ -7,7 +7,18 @@ namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
     {
         public void Configure(EntityTypeBuilder<Mentor> builder)
         {
-         
+            builder.HasMany(d => d.Students)
+               .WithMany(p => p.Mentors)
+               .UsingEntity<MentorStudent>(
+                   dp => dp.HasOne(p => p.Student)
+                       .WithMany()
+                       .HasForeignKey(dp => dp.StudentId),
+                   dp => dp.HasOne(d => d.Mentor)
+                       .WithMany()
+                       .HasForeignKey(dp => dp.MentorId)
+                       .OnDelete(DeleteBehavior.ClientCascade),
+                   dp => { dp.HasKey(x => new { x.StudentId, x.MentorId }); }
+               );
         }
     }
 }
