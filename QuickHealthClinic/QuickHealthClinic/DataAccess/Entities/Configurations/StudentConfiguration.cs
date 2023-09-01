42882d87f572b331f6bc3b21b50ba30362c7c1ed
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 16:56:16 +0200

    StudentConfiguration now implements after IEntityTypeConfiguration interface

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/StudentConfiguration.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/StudentConfiguration.cs
@@ -1,6 +1,13 @@
-﻿namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
 {
-    public class StudentConfiguration
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
     {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
