8d8812a662e24204408a91f47ba713c4d3e1874a
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 16:46:32 +0200

    MentorConfiguration now implements after IEntityTypeConfiguration

---
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/MentorConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
+{
+    public class MentorConfiguration : IEntityTypeConfiguration<Mentor>
+    {
+        public void Configure(EntityTypeBuilder<Mentor> builder)
+        {
+        }
+    }
+}
