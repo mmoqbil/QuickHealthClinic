d3243a7e533f7e590df7c398d5e60165ef0d39f3
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 17:02:57 +0200

    Filled ClinicMentor with fields and properties to bond clinics with mentors

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/ClinicMentor.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/ClinicMentor.cs
@@ -2,5 +2,9 @@
 {
     public class ClinicMentor
     {
+        public int MentorId { get; set; }
+        public Mentor Mentor { get; set; }
+        public int ClinicId { get; set; }
+        public Clinic Clinic { get; set; }
     }
 }
