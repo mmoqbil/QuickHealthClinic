26edee797059cd4be5267c64c2a8e83f8bb047fe
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 17:04:42 +0200

    Filled ClinicStudent with clinic and student id properties and fields

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/ClinicStudent.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/ClinicStudent.cs
@@ -2,5 +2,9 @@
 {
     public class ClinicStudent
     {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int ClinicId { get; set; }
+        public Clinic Clinic { get; set; }
     }
 }
