af7c2c3e6110535ad75c11102d133648e93df721
Mateusz <mateusz.moqbil@gmail.com>
2023-09-01 16:58:09 +0200

    ClinicConfiguration now implements after IEntityTypeConfiguration interface

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/ClinicConfiguration.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/ClinicConfiguration.cs
@@ -1,6 +1,13 @@
-﻿namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
 {
-    public class ClinicConfiguration
+    public class ClinicConfiguration : IEntityTypeConfiguration<Clinic>
     {
+        public void Configure(EntityTypeBuilder<Clinic> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
