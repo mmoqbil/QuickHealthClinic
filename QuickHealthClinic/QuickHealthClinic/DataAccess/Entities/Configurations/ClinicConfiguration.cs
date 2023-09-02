55314a33b88bf5e3695506cf86265321a621e62c
Mateusz <mateusz.moqbil@gmail.com>
2023-09-02 11:24:34 +0200

    Created relation between Clinic and Student using fluent api and entity framework

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/ClinicConfiguration.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/ClinicConfiguration.cs
@@ -7,7 +7,18 @@ namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
     {
         public void Configure(EntityTypeBuilder<Clinic> builder)
         {
-            throw new NotImplementedException();
+            builder.HasMany(c => c.Students)
+            .WithMany(p => p.Clinics)
+            .UsingEntity<ClinicStudent>(
+                cp => cp.HasOne(p => p.Student)
+                    .WithMany()
+                    .HasForeignKey(cp => cp.StudentId),
+                cp => cp.HasOne(c => c.Clinic)
+                    .WithMany()
+                    .HasForeignKey(cp => cp.ClinicId)
+                    .OnDelete(DeleteBehavior.ClientCascade),
+                cp => { cp.HasKey(x => new { x.StudentId, x.ClinicId }); }
+            );
         }
     }
 }
