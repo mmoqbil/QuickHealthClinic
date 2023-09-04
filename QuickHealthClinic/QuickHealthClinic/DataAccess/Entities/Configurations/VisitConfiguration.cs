58b79d8d983cc01c02b8c9c7354277f5c1c2d23b
Mateusz <mateusz.moqbil@gmail.com>
2023-09-04 14:11:18 +0200

    VisitConfiguration now implements after IEntityTypeConfiguration interface

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/VisitConfiguration.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Configurations/VisitConfiguration.cs
@@ -1,6 +1,13 @@
-﻿namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuickLifeCoachingClinic.DataAccess.Entities.Configurations
 {
-    public class VisitConfiguration
+    public class VisitConfiguration : IEntityTypeConfiguration<Visit>
     {
+        public void Configure(EntityTypeBuilder<Visit> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
