5bbed5136c3a7dcf79d17b273ce53dba220935e3
Mateusz <mateusz.moqbil@gmail.com>
2023-09-06 19:52:56 +0200

    Implemented VisitDto with all needed dto properties like id, student or duration

--- QuickHealthClinic/QuickHealthClinic/DTOs/VisitDtoFolder/VisitDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/VisitDtoFolder/VisitDto.cs
@@ -1,6 +1,13 @@
 ﻿namespace QuickLifeCoachingClinic.DTOs.VisitDtoFolder
 {
-    public class VisitDto
+    public class VisitDTO
     {
+        public int Id { get; set; }
+        public long StartDate { get; set; }
+        public string Treatment { get; init; }
+        public string Student { get; init; }
+        public int StudentId { get; init; }
+        public int Duration { get; set; }
+        public bool? Confirmed { get; set; }
     }
 }
