4d70b5b7464a1ce9a5703531548dbd394d8a51ae
Mateusz <mateusz.moqbil@gmail.com>
2023-09-06 19:45:47 +0200

    Implemented AcceptVisit to turn visit to confirmed in VisitService

--- QuickHealthClinic/QuickHealthClinic/Services/VisitServices/VisitService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/VisitServices/VisitService.cs
@@ -15,6 +15,15 @@ namespace QuickLifeCoachingClinic.Services.VisitServices
             _unitOfWork = unitOfWork;
         }
 
+        public async Task<bool> AcceptVisit(int visitId)
+        {
+            var visit = await _unitOfWork.VisitRepository.GetAsync(visitId);
+            if (visit == null) return false;
+
+            visit.Confirmed = true;
+            await _unitOfWork.SaveAsync();
+            return true;
+        }
         public async Task<IEnumerable<VisitCalendarDto>> GetVisitsForMonth(int mentorId, DateOnly date)
         {
             return await _unitOfWork.VisitRepository.GetVisitsForMonth(mentorId, date);
