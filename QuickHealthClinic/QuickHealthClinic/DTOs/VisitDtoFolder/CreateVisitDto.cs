4b03ed915fd3b008070e8715776d771b2e97bf51
Mateusz <mateusz.moqbil@gmail.com>
2023-09-07 21:41:32 +0200

    Filled CreateVisitDto with all dto's properties

--- QuickHealthClinic/QuickHealthClinic/DTOs/VisitDtoFolder/CreateVisitDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/VisitDtoFolder/CreateVisitDto.cs
@@ -2,5 +2,10 @@
 {
     public class CreateVisitDto
     {
+        public string Name { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime VisitDate { get; set; }
+        public int Duration { get; set; }
     }
 }
