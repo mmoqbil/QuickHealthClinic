6f2a158809fa415529e4a7a78a054db39345cdf7
Mateusz <mateusz.moqbil@gmail.com>
2023-09-08 23:20:29 +0200

    Changed name from Patient to Student

--- QuickHealthClinic/QuickHealthClinic/DTOs/VisitDtoFolder/CreateVisitDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/VisitDtoFolder/CreateVisitDto.cs
@@ -3,7 +3,7 @@
     public class CreateVisitDto
     {
         public string Name { get; set; }
-        public int PatientId { get; set; }
+        public int StudentId { get; set; }
         public int DoctorId { get; set; }
         public DateTime VisitDate { get; set; }
         public int Duration { get; set; }
--- QuickHealthClinic/QuickHealthClinic/Services/VisitServices/VisitService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/VisitServices/VisitService.cs
@@ -80,5 +80,7 @@ namespace QuickLifeCoachingClinic.Services.VisitServices
 
             return visitsDTO;
         }
+
+
     }
 }
