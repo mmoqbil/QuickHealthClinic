f0efa3e6aa1991e7d09e2630cbcd18d324d2f0cb
Mateusz <mateusz.moqbil@gmail.com>
2023-09-09 15:51:27 +0200

    Filled WorkHours class with all needed properties and Mentor field

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/WorkHours.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/WorkHours.cs
@@ -2,5 +2,11 @@
 {
     public class WorkHours
     {
+        public int Id { get; set; }
+        public int MentorId { get; set; }
+        public virtual Mentor? Mentor { get; set; }
+        public string Day { get; set; }
+        public string StartHour { get; set; }
+        public string EndHour { get; set; }
     }
 }
