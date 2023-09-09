483c0f4ab5bff38f3475e4010e1336f4195f3d8d
Mateusz <mateusz.moqbil@gmail.com>
2023-09-09 15:49:52 +0200

    Filled Referral with all properties and student field

--- QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Referral.cs
+++ QuickHealthClinic/QuickHealthClinic/DataAccess/Entities/Referral.cs
@@ -2,5 +2,12 @@
 {
     public class Referral
     {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public DateTime Created { get; set; }
+        public string Specialist { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
     }
 }
