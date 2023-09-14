0ab0e0066c190c19a9def4c9c3b074d022a45c3c
Mateusz <mateusz.moqbil@gmail.com>
2023-09-14 21:00:25 +0200

    Created ReferralControler

---
+++ QuickHealthClinic/QuickHealthClinic/Controllers/ReferralController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickLifeCoachingClinic.Controllers
+{
+    public class ReferralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
