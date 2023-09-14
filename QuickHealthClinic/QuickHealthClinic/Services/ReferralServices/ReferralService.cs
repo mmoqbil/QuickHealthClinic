295076c5b9104d7445f59700ac81c58de11d5b60
Mateusz <mateusz.moqbil@gmail.com>
2023-09-14 20:45:52 +0200

    Added mapper field

--- QuickHealthClinic/QuickHealthClinic/Services/ReferralServices/ReferralService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/ReferralServices/ReferralService.cs
@@ -1,9 +1,11 @@
-﻿using QuickLifeCoachingClinic.DTOs.ReferralDtoFolder;
+﻿using AutoMapper;
+using QuickLifeCoachingClinic.DTOs.ReferralDtoFolder;
 
 namespace QuickLifeCoachingClinic.Services.ReferralServices
 {
     public class ReferralService : IReferralService
     {
+        private readonly IMapper _mapper;
         public Task<IEnumerable<ReferralDto>> GetIdAsync(int id)
         {
             throw new NotImplementedException();
