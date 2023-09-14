cc8ba787c2370b75eab1be0313beade645157ac4
Mateusz <mateusz.moqbil@gmail.com>
2023-09-14 20:45:29 +0200

    ReferralService now implements after IReferralService interface

--- QuickHealthClinic/QuickHealthClinic/Services/ReferralServices/ReferralService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/ReferralServices/ReferralService.cs
@@ -1,6 +1,12 @@
-﻿namespace QuickLifeCoachingClinic.Services.ReferralServices
+﻿using QuickLifeCoachingClinic.DTOs.ReferralDtoFolder;
+
+namespace QuickLifeCoachingClinic.Services.ReferralServices
 {
-    public class ReferralService
+    public class ReferralService : IReferralService
     {
+        public Task<IEnumerable<ReferralDto>> GetIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
