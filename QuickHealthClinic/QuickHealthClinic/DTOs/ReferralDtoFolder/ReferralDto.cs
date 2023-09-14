1291cd443e3ca62ec07493c2dba3638cece5224d
Mateusz <mateusz.moqbil@gmail.com>
2023-09-14 20:56:51 +0200

    Declared all dtos properties in ReferralDto class

--- QuickHealthClinic/QuickHealthClinic/DTOs/ReferralDtoFolder/ReferralDto.cs
+++ QuickHealthClinic/QuickHealthClinic/DTOs/ReferralDtoFolder/ReferralDto.cs
@@ -2,5 +2,12 @@
 {
     public class ReferralDto
     {
+        public int Id { get; set; }
+        public string Patient { get; set; }
+        public int PatientId { get; init; }
+        public long Date { get; set; }
+        public string Specialist { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
     }
 }
