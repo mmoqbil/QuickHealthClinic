8bbdf60d4ba30b8d8e66c9a9436148f3a8531994
Mateusz <mateusz.moqbil@gmail.com>
2023-09-14 20:56:05 +0200

    Refactoring ReferralService

--- QuickHealthClinic/QuickHealthClinic/Services/ReferralServices/ReferralService.cs
+++ QuickHealthClinic/QuickHealthClinic/Services/ReferralServices/ReferralService.cs
@@ -17,20 +17,7 @@ namespace QuickLifeCoachingClinic.Services.ReferralServices
 
         public async Task<IEnumerable<ReferralDto>> GetIdAsync(int id)
         {
-            var referrals = await _unitOfWork
-
-            var referralsDto = referrals.Select(r => new ReferralDto
-            {
-                Id = r.Id,
-                PatientId = r.PatientId,
-                Patient = $"{r.Patient.FirstName} {r.Patient.LastName}",
-                Date = new DateTimeOffset(r.Created).ToUnixTimeSeconds(),
-                Specialist = r.Specialist,
-                Description = r.Description,
-                Code = r.Code
-            });
-
-            return referralsDto;
+           
         }
     }
 }
